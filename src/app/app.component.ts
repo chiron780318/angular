9458a700a0fdc028277807e553ef8072cc6a92e4
Chiron <chiahsing.chu@frog-jump.com>
2025-08-06 17:42:35 +0800

    隨便練習

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserComponent } from './user/user.component';
+import { Child } from './child/child';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UserComponent, Child],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Test1';
+  isEditable = true;
+  message = '';
+
+  greet() {
+    this.message = 'Hello, there 👋';
+  }
+
+  items = new Array();
+
+  addItem(item: string) {
+    this.items.push(item);
+  }
 }
---
+++ src/app/user/user.component.ts
@@ -0,0 +1,11 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  name = input<string>();
+}
